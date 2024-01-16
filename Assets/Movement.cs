95cff7f50c96f2c58a158c2328f57d4d13986154
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2024-01-16 15:37:37 +0100

    Added Option to Disable Character 1 by pressing 2

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -16,6 +16,8 @@ public class Movement : MonoBehaviour
 
     bool movingHorizontal;
 
+    bool disabled = false;
+
     Animator animator;
 
     void Start()
@@ -26,51 +28,59 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            disabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)){
+            disabled = true;
+        }
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
-        if (Mathf.Abs(moveX) > 0 && Mathf.Abs(moveY) > 0){
-            moveY = 0;
-        }
-        if (moveX > 0){
-            animator.Play("GreenCapWalkingRight");
-            facingRight = true;
-            movingHorizontal = true;
-        }
-        else if (moveX < 0){
-            animator.Play("GreenCapWalkingLeft");
-            facingRight = false;
-            movingHorizontal = true;
-        }
-        else if (moveY > 0){
-            animator.Play("GreenCapWalkingUp");
-            facingUp = true;
-            movingHorizontal = false;
-        }
-        else if (moveY < 0){
-            animator.Play("GreenCapWalkingDown");
-            facingUp = false;
-            movingHorizontal = false;
-        }
-        else{
-            if (movingHorizontal == true){
-                if (facingRight == true){
-                    animator.Play("GreenCapIdleRight");
-                }
-                else{
-                    animator.Play("GreenCapIdleLeft");
-                }
+        if (disabled == false){
+            if (Mathf.Abs(moveX) > 0 && Mathf.Abs(moveY) > 0){
+                moveY = 0;
+            }
+            if (moveX > 0){
+                animator.Play("GreenCapWalkingRight");
+                facingRight = true;
+                movingHorizontal = true;
+            }
+            else if (moveX < 0){
+                animator.Play("GreenCapWalkingLeft");
+                facingRight = false;
+                movingHorizontal = true;
+            }
+            else if (moveY > 0){
+                animator.Play("GreenCapWalkingUp");
+                facingUp = true;
+                movingHorizontal = false;
+            }
+            else if (moveY < 0){
+                animator.Play("GreenCapWalkingDown");
+                facingUp = false;
+                movingHorizontal = false;
             }
             else{
-                if (facingUp == true){
-                    animator.Play("GreenCapIdleUp");
+                if (movingHorizontal == true){
+                    if (facingRight == true){
+                        animator.Play("GreenCapIdleRight");
+                    }
+                    else{
+                        animator.Play("GreenCapIdleLeft");
+                    }
                 }
                 else{
-                    animator.Play("GreenCapIdleDown");
+                    if (facingUp == true){
+                        animator.Play("GreenCapIdleUp");
+                    }
+                    else{
+                        animator.Play("GreenCapIdleDown");
+                    }
                 }
             }
+            Vector3 movement = new(moveX, moveY, 0);
+            transform.Translate(movement * Time.deltaTime * speed);
         }
 
-        Vector3 movement = new(moveX, moveY, 0);
-        transform.Translate(movement * Time.deltaTime * speed);
     }
 }
